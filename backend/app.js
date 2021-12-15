751cb20717a8c17fd75470ebade9f9e5f1064d28
Charis <charis.petsas96@gmail.com>
2021-12-15 16:55:00 +0000

    first commit

---
+++ backend/app.js
@@ -0,0 +1,30 @@
+const http = require('http')
+const express = require('express')
+const bodyParser = require('body-parser')
+const host = process.env.HOST
+const port = process.env.PORT
+
+var index = require('./routes/index')
+var models = require('./models')
+
+models.sequelize.sync({alter: true})
+const formData = require("express-form-data");
+
+let app=express()
+
+app.use(formData.parse());
+app.use(formData.format());
+app.use(formData.stream());
+app.use(formData.union());
+
+app.use(bodyParser.json());
+app.use(express.urlencoded());
+app.get('/',function (req, res){
+    res.send('Something went wrong')
+})
+
+app.use('/', index)
+
+let server = app.listen(port, function(){
+    console.log('Server running')
+})
